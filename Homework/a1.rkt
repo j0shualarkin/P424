32100c3696682df3e1ac5947e62051b51a1f3fab
Fred Fu <moonsolo@gmail.com>
2018-08-25 16:57:28 -0400

    finished hw1

--- Homework/a1.rkt
+++ Homework/a1.rkt
@@ -2,7 +2,8 @@
 
 (require rackunit
          (for-syntax syntax/parse)
-         2htdp/batch-io)
+         2htdp/batch-io
+         graph)
 
 
 #;(define (graph-write graph port mode)
@@ -20,13 +21,7 @@
 (define (slice lst start end)
   (take (drop lst start) (- end start)))
 
-(slice '(1 2 3) 1 (- (length '(1 2 3)) 1))
-
 (require racket/list)
-
-
-
-
 
 
 
@@ -42,33 +37,12 @@
                           (F E)
                           )))
 
-(define test1
-  '((A (B C))
-    (B (A C))
-    (C (A B))))
-
-#;
-(define hmm
-  '((A (B C))
-    (B (A C))
-    (C (A B))))
-
-(define test2
-  '((A (B C D))
-    (B (E C))
-    (C ())
-    (D (C F))
-    (E ())
-    (F (E))))
-
 ;; read-file :: (U File String) -> Graph
 ;; converts a file of s-expression represented graph
-;; to the host graph representation 
-(define (read-graph st)  
+;; to the host graph representation
+(define (read-graph st)
   ;; input : String
-  (define input (if (input-port? st)
-                    (read st)
-                    st))
+  (define input (read st))
   (graph
    (for/list ([e input])
      (cons (car e) (cadr e)))))
@@ -77,45 +51,40 @@
 (define (graph=? G1 G2)
   (equal? (graph-edges G1) (graph-edges G2)))
 
-(check graph=? (read-graph test1) triangle)
-(check graph=? (read-graph test2) 7verts)
-
+(check graph=? (read-graph (open-input-file "graph-test-fixtures")) triangle)
+(check graph=? (read-graph (open-input-file "graph-test-fixtures2")) 7verts)
 
 ;; print-graph : Graph -> Void
 (define print-graph
   (λ (G)
     (define data (graph-edges G))
     (define final (last data))
-    
+
     (display "(")
     (displayln (cons (caar data) (list (cdar data))))
 
     (for ([i (slice data 1 (- (length data) 1))])
       (display " ")
       (displayln (cons (car i) (list (cdr i)))))
-    
+
     (display " ")
     (display (cons (car final) (list (cdr final))))
     (displayln ")")))
 
-(print-graph 7verts)
-
-
-(define (foo xss)
-  (for/list ([xs xss])
-        (for/list ([ys (cdr xs)])
-           (cons (car xs) (list ys)))))
-
-(check-equal?
- (foo '((A B C D) (B E C)))
-'((A B)(A C) (A D) (B E) (B C))
-)
-
-(require graph)
 
 (define (spanning-tree G)
-  (mst-kruskal (directed-graph (graph-edges G))))
-#;
-(spanning-tree 7verts)
+  (define seen `(,(caar (graph-edges G))))
+  (graph (for/list ([i (graph-edges G)])
+           (cons (car i)
+                 (for/list ([vt (cdr i)] #:when (not (memv vt seen)))
+                   (begin (set! seen (cons vt seen))
+                          vt))))))
 
+(define G2 (graph '((A C D)
+                    (B E)
+                    (C D E)
+                    (D A B)
+                    (E))))
 
+(check-equal? (graph-edges (spanning-tree 7verts)) '((A B C D) (B E) (C) (D F) (E) (F)))
+(check-equal? (graph-edges (spanning-tree G2)) '((A C D) (B E) (C) (D B) (E)))
