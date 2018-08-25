eabbe58140b3bc38f5abc293b26674ff1c3e3b5f
Joshua Larkin <joslarki@iu.edu>
2018-08-25 16:05:03 -0400

    working on spanning-tree

--- Homework/a1.rkt
+++ Homework/a1.rkt
@@ -1,10 +1,32 @@
 #lang racket
 
 (require rackunit
-         #;(for-syntax syntax/parse)
+         (for-syntax syntax/parse)
          2htdp/batch-io)
 
+
+#;(define (graph-write graph port mode)
+  (write-string "(" port)
+  (for ([e (graph-edges graph)])
+    (write-string (format "~a\n" (cons (car e) (list (cdr e))))))
+  (write-string ")" port))
+#;
+(struct graph [edges]
+  #:methods gen:custom-write
+  ((define write-proc graph-write)))
+
 (struct graph [edges])
+
+(define (slice lst start end)
+  (take (drop lst start) (- end start)))
+
+(slice '(1 2 3) 1 (- (length '(1 2 3)) 1))
+
+(require racket/list)
+
+
+
+
 
 
 
@@ -20,39 +42,80 @@
                           (F E)
                           )))
 
-(define test1 '((A (B C))
-                (B (A C))
-                (C (A B))))
+(define test1
+  '((A (B C))
+    (B (A C))
+    (C (A B))))
 
-(define test2 '((A (B C D))
-                (B (E C))
-                (C ())
-                (D (C F))
-                (E ())
-                (F (E))))
+#;
+(define hmm
+  '((A (B C))
+    (B (A C))
+    (C (A B))))
 
-;; read-file :: File -> Graph
+(define test2
+  '((A (B C D))
+    (B (E C))
+    (C ())
+    (D (C F))
+    (E ())
+    (F (E))))
+
+;; read-file :: (U File String) -> Graph
 ;; converts a file of s-expression represented graph
 ;; to the host graph representation 
-(define (read-graph file)
+(define (read-graph st)  
   ;; input : String
-  (define input (read-file file))
-  file)
+  (define input (if (input-port? st)
+                    (read st)
+                    st))
+  (graph
+   (for/list ([e input])
+     (cons (car e) (cadr e)))))
 
 
-#;(define-syntax (read-graph^ stx)
-  (syntax-parse stx
-    ((_ (v0 ())) #'(v0))
-    ((_ (v0 (v1 ...))) #'(v0 v1 ...))
-    ((_ (v0 (v1 ...))
-        ...) #`(graph ((v0 v1 ...)
-                       #,(read-graph^ #'...))))))
+(define (graph=? G1 G2)
+  (equal? (graph-edges G1) (graph-edges G2)))
+
+(check graph=? (read-graph test1) triangle)
+(check graph=? (read-graph test2) 7verts)
 
 
-(check-equal? (read-graph test1) triangle)
-(check-equal? (read-graph test2) 7verts)
+;; print-graph : Graph -> Void
+(define print-graph
+  (λ (G)
+    (define data (graph-edges G))
+    (define final (last data))
+    
+    (display "(")
+    (displayln (cons (caar data) (list (cdar data))))
+
+    (for ([i (slice data 1 (- (length data) 1))])
+      (display " ")
+      (displayln (cons (car i) (list (cdr i)))))
+    
+    (display " ")
+    (display (cons (car final) (list (cdr final))))
+    (displayln ")")))
+
+(print-graph 7verts)
 
 
+(define (foo xss)
+  (for/list ([xs xss])
+        (for/list ([ys (cdr xs)])
+           (cons (car xs) (list ys)))))
 
+(check-equal?
+ (foo '((A B C D) (B E C)))
+'((A B)(A C) (A D) (B E) (B C))
+)
+
+(require graph)
+
+(define (spanning-tree G)
+  (mst-kruskal (directed-graph (graph-edges G))))
+#;
+(spanning-tree 7verts)
 
 
