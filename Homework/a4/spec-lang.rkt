ac767b1a75a6dcfedc8e5eb6a38cc878be800051
Fred Fu <moonsolo@gmail.com>
2018-09-22 12:23:07 -0400

    spec done

--- Homework/a4/spec-lang.rkt
+++ Homework/a4/spec-lang.rkt
@@ -17,20 +17,25 @@
     (define-syntax-class fields
       (pattern f:id #:fail-unless (member #'f (list #'author #'university #'name) free-identifier=?) "not name...")))
 
-  (define-syntax (#%module-begin stx)
+  (define-for-syntax seen (make-hash '((name . #f) (author . #f) (university . #f))))
+  (define-syntax (do-module-begin stx)
     (syntax-parse stx
-      ((_ e ...) #'(modbeg
-                    (provide name author university)
-                    e ...))))
-  
+      [(_)
+       (for ([(k v) seen])
+         (unless v
+           (error (format "~a isn't defined" k))))
+       #'(void)]
+      [(_ e1 e ...) #'(begin e1 (do-module-begin e ...))]))
+
+  (define-syntax-rule (#%module-begin e ...)
+    (modbeg (do-module-begin e ...)))
+
   (define-syntax (our-define stx)
     (syntax-parse stx
-      [(_ id1:fields id2:id) #`(define id1 (quote #,(syntax->datum #'id2)))])
-    )
-#;#;#;
-  (define name j)
-  (define author a)
-  (define university u))
+      [(_ id1:fields id2:id)
+       (hash-set! seen (syntax->datum #'id1) #t)
+       #`(define id1 (quote #,(syntax->datum #'id2)))])
+    ))
 
 ;; all top level forms are (define id1 id2)
 ;; quotes all identifier on RHS
@@ -42,4 +47,12 @@
   author
   (define university indiana)
   university
+  )
+
+#;
+(module test (submod ".." spec)
+  (define name Froshua)
+  name
+  (define author samtthias)
+  author
   )
