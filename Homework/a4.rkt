f1fadf2444260d5e5e39141a1ec0f28ec3be2e69
Fred Fu <moonsolo@gmail.com>
2018-09-22 10:48:20 -0400

    all Encode

--- Homework/a4.rkt
+++ Homework/a4.rkt
@@ -5,10 +5,15 @@
            (rename-in racket [#%datum orig-datum]))
   (define-syntax (my-datum stx)
     (syntax-parse stx
-      [(_ . a:number) #`(orig-datum . #,(string (integer->char (syntax-e #'a))))])))
+      [(_ . a:number)
+       (define v (syntax-e #'a))
+       (if (or (<= 65 v 90) (<= 97 v 122))
+           #`(orig-datum . #,(string (integer->char v)))
+           (raise-syntax-error 'datum "not valid number ~v" #'a))])))
 
 (module test (submod ".." Encode)
   65
   66
   67
-  68)
+  68
+  50)
