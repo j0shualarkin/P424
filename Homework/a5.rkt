acd344a4ef1b7fc3a2ff300b03431367795778ab
Fred Fu <moonsolo@gmail.com>
2018-10-09 12:06:51 -0400

    let's parse a Racket expression in the standard way
    Suddenly remembered I am a PL student and have took Dan's class ...

--- Homework/a5.rkt
+++ Homework/a5.rkt
@@ -8,27 +8,33 @@
   (define-syntax-class const
     (pattern c:number)
     (pattern c:string)
-    (pattern c:boolean)))
+    (pattern c:boolean))
+  (define (subst stx var-id const-expr)
 
-(define-for-syntax (replace stx id const-expr)
-  (cond
-    [(and (identifier? stx) (free-identifier=? stx id)) const-expr]
-    [else (let ([stx-lst (syntax->list stx)])
-            (if stx-lst
-                (datum->syntax stx (map (λ (stx)
-                                          (replace stx id const-expr))
-                                        stx-lst))
-                stx))]))
+    (syntax-parse stx #:literals (λ)
+      [var:id #:when (free-identifier=? #'var var-id) const-expr]
+      [(λ (x:id ...) body) #`(λ (x ...) #,(subst #'body var-id const-expr))]
+      [(e0 er ...) (define data (map (λ (x) (subst x var-id const-expr))
+                                     (syntax->list #'(e0 er ...))))
+                  #`(#,@data)]
+      [_ stx])))
+
+
 
 (define-syntax (let stx)
   (syntax-parse stx
     [(_ ([x:id rhs:const] binding ...) body)
-     (define body^ (syntax-e (replace #'body #'x #'rhs)))
+     (define body^ (syntax-e (subst #'body #'x #'rhs)))
      #`(let (binding ...) #,body^)]
     [(_ ([x:id rhs:expr] ...) body) #'(orig-let ([x rhs] ...)
                                         body)]))
 
-(let ([x 10]
-      [y 20]
-      [z "20"])
-  (+ x y (string->number z)))
+(module+ test
+  (require rackunit)
+  (check-equal? (let ([x 10]
+                      [y 20]
+                      [z "20"])
+                  ((λ (m n) (+ 50 m n x y))
+                   (+ x y (string->number z))
+                   100))
+                230))
